7adc3186937e08d9406b6a99c346a35f2b219e6f
yush <yush2202@gmail.com>
2026-03-06 19:45:41 +0900

    로그 추가

--- src/main/java/coupon/ReadOnlyDataSourceRouter.java
+++ src/main/java/coupon/ReadOnlyDataSourceRouter.java
@@ -1,15 +1,19 @@
 package coupon;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+@Slf4j
 public class ReadOnlyDataSourceRouter extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
         if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            log.info("🚩 현재 선택된 라우팅 키: {}", "SLAVE");
             return DataSourceType.READER;
         }
+        log.info("🚩 현재 선택된 라우팅 키: {}", "MASTER");
         return DataSourceType.WRITER;
     }
 }
